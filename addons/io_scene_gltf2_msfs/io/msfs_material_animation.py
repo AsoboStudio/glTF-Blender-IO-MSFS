e16520da349ef217f1d089d5be3c35ef482424de
pepperoni505 <pepperoni505@gmail.com>
2022-03-07 21:44:54 -0500

    refactor: format

--- addons/io_scene_gltf2_msfs/io/msfs_material_animation.py
+++ addons/io_scene_gltf2_msfs/io/msfs_material_animation.py
@@ -48,9 +48,15 @@ class MSFSMaterialAnimation:
             if material.animation_data is not None:
                 if blender_action == material.animation_data.action:
                     return material
-                elif export_settings['gltf_nla_strips'] is True: # Check if the animation is an NLA strip
+                elif (
+                    export_settings["gltf_nla_strips"] is True
+                ):  # Check if the animation is an NLA strip
                     for track in material.animation_data.nla_tracks:
-                        non_muted_strips = [strip for strip in track.strips if strip.action is not None and strip.mute is False]
+                        non_muted_strips = [
+                            strip
+                            for strip in track.strips
+                            if strip.action is not None and strip.mute is False
+                        ]
                         if track.strips is None or len(non_muted_strips) != 1:
                             continue
                         for strip in non_muted_strips:
@@ -58,7 +64,9 @@ class MSFSMaterialAnimation:
                                 return material
 
     @staticmethod
-    def gather_actions(blender_object, blender_actions, blender_tracks, action_on_type, export_settings):
+    def gather_actions(
+        blender_object, blender_actions, blender_tracks, action_on_type, export_settings
+    ):
         """
         EXPORT
         Based off code in the Khronos glTF exporter. This looks through all the materials in the object and checks
@@ -71,8 +79,11 @@ class MSFSMaterialAnimation:
         :return:
         """
         # First step is to get a list of all material animation actions and NLA tracks (if used)
-        if not (blender_object.type == "MESH" and blender_object.data is not None
-                    and len(blender_object.material_slots) > 0):
+        if not (
+            blender_object.type == "MESH"
+            and blender_object.data is not None
+            and len(blender_object.material_slots) > 0
+        ):
             return
 
         for material_slot in blender_object.material_slots:
@@ -87,20 +98,30 @@ class MSFSMaterialAnimation:
                 action_on_type[material.animation_data.action.name] = "MATERIAL"
 
             # Collect associated strips from NLA tracks
-            if export_settings['gltf_nla_strips'] is True:
+            if export_settings["gltf_nla_strips"] is True:
                 for track in material.animation_data.nla_tracks:
                     # Multi-strip tracks do not export correctly yet (they need to be baked),
                     # so skip them for now and only write single-strip tracks.
-                    non_muted_strips = [strip for strip in track.strips if strip.action is not None and strip.mute is False]
+                    non_muted_strips = [
+                        strip
+                        for strip in track.strips
+                        if strip.action is not None and strip.mute is False
+                    ]
                     if track.strips is None or len(non_muted_strips) != 1:
                         continue
                     for strip in non_muted_strips:
                         blender_actions.append(strip.action)
-                        blender_tracks[strip.action.name] = track.name # Always set after possible active action -> None will be overwrite
+                        blender_tracks[
+                            strip.action.name
+                        ] = (
+                            track.name
+                        )  # Always set after possible active action -> None will be overwrite
                         action_on_type[strip.action.name] = "MATERIAL"
 
     @staticmethod
-    def replace_channel_target(gltf2_animation_channel_target, channels, blender_object, export_settings):
+    def replace_channel_target(
+        gltf2_animation_channel_target, channels, blender_object, export_settings
+    ):
         """
         EXPORT
         Replace targets for channels that are material animations. We don't use the target object for material targets, instead we
@@ -114,17 +135,23 @@ class MSFSMaterialAnimation:
         :return:
         """
         for channel in channels:
-            blender_material = MSFSMaterialAnimation.get_material_from_action(blender_object, channel.id_data, export_settings)
+            blender_material = MSFSMaterialAnimation.get_material_from_action(
+                blender_object, channel.id_data, export_settings
+            )
             if not blender_material:
                 continue
 
-            gltf2_animation_channel_target.path = blender_material.path_resolve(channel.data_path.split(".")[0])
+            gltf2_animation_channel_target.path = blender_material.path_resolve(
+                channel.data_path.split(".")[0]
+            )
 
             # Set data path to value in order to force the Khronos exporter to gather keyframes
             channel.data_path = "value"
-    
+
     @staticmethod
-    def add_placeholder_channel(gltf2_animation, blender_action, blender_object, export_settings):
+    def add_placeholder_channel(
+        gltf2_animation, blender_action, blender_object, export_settings
+    ):
         """
         EXPORT
         If we have a glTF animation with only material animations, we need to create a placeholder scale channel. Because we utilize the `extensions`
@@ -137,7 +164,9 @@ class MSFSMaterialAnimation:
         :return:
         """
         for fcurve in blender_action.fcurves:
-            material = MSFSMaterialAnimation.get_material_from_action(blender_object, blender_action, export_settings)
+            material = MSFSMaterialAnimation.get_material_from_action(
+                blender_object, blender_action, export_settings
+            )
 
             if material is None:
                 # If we actually find a property besides the material animations, we don't need a temp fcurve
@@ -149,10 +178,11 @@ class MSFSMaterialAnimation:
         fcurve = temp_action.fcurves.new(data_path="scale", index=0)
         fcurve.keyframe_points.add(1)
 
-
         # Collect temp channel and cleanup
         gltf2_animation.channels.extend(
-            gltf2_blender_gather_animation_channels.gather_animation_channels(temp_action, blender_object, export_settings)
+            gltf2_blender_gather_animation_channels.gather_animation_channels(
+                temp_action, blender_object, export_settings
+            )
         )
 
         bpy.data.actions.remove(temp_action)
@@ -174,19 +204,18 @@ class MSFSMaterialAnimation:
             if type(channel.target.path.id_data) != bpy.types.Material:
                 continue
 
-            material_animation_channels.append({
-                "sampler": channel.sampler,
-                "target": channel.target.path
-            })
+            material_animation_channels.append(
+                {"sampler": channel.sampler, "target": channel.target.path}
+            )
             gltf2_animation.channels.pop(i)
 
         if material_animation_channels:
-            gltf2_animation.extensions[MSFSMaterialAnimation.extension_name] = Extension(
+            gltf2_animation.extensions[
+                MSFSMaterialAnimation.extension_name
+            ] = Extension(
                 name=MSFSMaterialAnimation.extension_name,
-                extension={
-                    "channels": material_animation_channels
-                },
-                required=False
+                extension={"channels": material_animation_channels},
+                required=False,
             )
 
     @staticmethod
@@ -202,45 +231,74 @@ class MSFSMaterialAnimation:
         """
         if not gltf2_animation.extensions:
             return
-        
-        if MSFSMaterialAnimation.extension_name not in gltf2_animation.extensions.keys():
+
+        if (
+            MSFSMaterialAnimation.extension_name
+            not in gltf2_animation.extensions.keys()
+        ):
             return
 
-        for channel in gltf2_animation.extensions[MSFSMaterialAnimation.extension_name]["channels"]:
+        for channel in gltf2_animation.extensions[MSFSMaterialAnimation.extension_name][
+            "channels"
+        ]:
             material_index = None
             for j, material in enumerate(gltf2_plan.materials):
                 if material.name == channel["target"].id_data.name:
                     material_index = j
                     break
-            
+
             if material_index is None:
                 continue
 
             target_property = channel["target"].path_from_id().split(".")[0]
 
             if target_property == "msfs_base_color_factor":
-                channel["target"] = f"materials/{material_index}/pbrMetallicRoughness/baseColorFactor"
+                channel[
+                    "target"
+                ] = f"materials/{material_index}/pbrMetallicRoughness/baseColorFactor"
             elif target_property == "msfs_emissive_factor":
                 channel["target"] = f"materials/{material_index}/emissiveFactor"
             elif target_property == "msfs_metallic_factor":
-                channel["target"] = f"materials/{material_index}/pbrMetallicRoughness/metallicFactor"
+                channel[
+                    "target"
+                ] = f"materials/{material_index}/pbrMetallicRoughness/metallicFactor"
             elif target_property == "msfs_roughness_factor":
-                channel["target"] = f"materials/{material_index}/pbrMetallicRoughness/roughnessFactor"
+                channel[
+                    "target"
+                ] = f"materials/{material_index}/pbrMetallicRoughness/roughnessFactor"
             elif target_property == "msfs_uv_offset_u":
-                channel["target"] = f"materials/{material_index}/extensions/ASOBO_material_UV_options/UVOffsetU"
+                channel[
+                    "target"
+                ] = f"materials/{material_index}/extensions/ASOBO_material_UV_options/UVOffsetU"
             elif target_property == "msfs_uv_offset_v":
-                channel["target"] = f"materials/{material_index}/extensions/ASOBO_material_UV_options/UVOffsetV"
+                channel[
+                    "target"
+                ] = f"materials/{material_index}/extensions/ASOBO_material_UV_options/UVOffsetV"
             elif target_property == "msfs_uv_tiling_u":
-                channel["target"] = f"materials/{material_index}/extensions/ASOBO_material_UV_options/UVTilingU"
+                channel[
+                    "target"
+                ] = f"materials/{material_index}/extensions/ASOBO_material_UV_options/UVTilingU"
             elif target_property == "msfs_uv_tiling_v":
-                channel["target"] = f"materials/{material_index}/extensions/ASOBO_material_UV_options/UVTilingV"
+                channel[
+                    "target"
+                ] = f"materials/{material_index}/extensions/ASOBO_material_UV_options/UVTilingV"
             elif target_property == "msfs_uv_rotation":
-                channel["target"] = f"materials/{material_index}/extensions/ASOBO_material_UV_options/UVRotation"
+                channel[
+                    "target"
+                ] = f"materials/{material_index}/extensions/ASOBO_material_UV_options/UVRotation"
             elif target_property == "msfs_wiper_1_state":
-                channel["target"] = f"materials/{material_index}/extensions/ASOBO_material_windshield_v2/wiper1State"
+                channel[
+                    "target"
+                ] = f"materials/{material_index}/extensions/ASOBO_material_windshield_v2/wiper1State"
             elif target_property == "msfs_wiper_2_state":
-                channel["target"] = f"materials/{material_index}/extensions/ASOBO_material_windshield_v2/wiper2State"
+                channel[
+                    "target"
+                ] = f"materials/{material_index}/extensions/ASOBO_material_windshield_v2/wiper2State"
             elif target_property == "msfs_wiper_3_state":
-                channel["target"] = f"materials/{material_index}/extensions/ASOBO_material_windshield_v2/wiper3State"
+                channel[
+                    "target"
+                ] = f"materials/{material_index}/extensions/ASOBO_material_windshield_v2/wiper3State"
             elif target_property == "msfs_wiper_4_state":
-                channel["target"] = f"materials/{material_index}/extensions/ASOBO_material_windshield_v2/wiper4State"
+                channel[
+                    "target"
+                ] = f"materials/{material_index}/extensions/ASOBO_material_windshield_v2/wiper4State"
