2db6d1368dd56be6817805e488c961823d324728
pepperoni505 <pepperoni505@gmail.com>
2022-03-28 22:10:19 -0400

    fix: also check blender image name for create image

--- addons/io_scene_gltf2_msfs/io/msfs_material.py
+++ addons/io_scene_gltf2_msfs/io/msfs_material.py
@@ -69,6 +69,8 @@ class MSFSMaterial:
         # Find image created
         if pyimg.name in list(bpy.data.images):
             return bpy.data.images[pyimg.name]
+        elif pyimg.blender_image_name in list(bpy.data.images):
+            return bpy.data.images[pyimg.blender_image_name]
         elif os.path.basename(pyimg.uri) in list(bpy.data.images):
             return bpy.data.images[pyimg.uri]
         elif "Image_%d" % index in list(bpy.data.images):
