517b42969e8a72e9e90e6ccfaaaefdd4fd48a813
Ron Haertel <in_house_it@outlook.com>
2023-07-20 15:42:52 -0400

    Fix ASOBO issue #255 Roughness node not connected in COPM material texture
    Node not connected ASOBO issue #255

--- addons/io_scene_gltf2_msfs/blender/msfs_material_function.py
+++ addons/io_scene_gltf2_msfs/blender/msfs_material_function.py
@@ -912,7 +912,7 @@ class MSFS_Material:
 
         elif nodeCompTex.image and not nodeDetailCompTex.image:
             nodeBlendCompMap.blend_type = "ADD"
-            self.link(nodeRoughnessScale.outputs[0], nodePrincipledBSDF.inputs[MSFS_PrincipledBSDFInputs.roughness.value])
+            self.link(nodeMulRoughness.outputs[0], nodePrincipledBSDF.inputs[MSFS_PrincipledBSDFInputs.roughness.value])
             self.link(nodeMulMetallic.outputs[0], nodePrincipledBSDF.inputs[MSFS_PrincipledBSDFInputs.metallic.value])
 
         elif not nodeCompTex.image and nodeDetailCompTex.image:
