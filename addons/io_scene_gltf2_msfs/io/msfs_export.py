2c6256e2cab7a23741d53be92aab4c6aa5d97381
Ron Haertel <in_house_it@outlook.com>
2023-07-21 15:28:31 -0400

    Update fix for Issue #252 Vertex color rainbow
    Face Corner - Byte color was causing rainbow effect - change to Face Corner - Color

--- addons/io_scene_gltf2_msfs/io/msfs_export.py
+++ addons/io_scene_gltf2_msfs/io/msfs_export.py
@@ -69,3 +69,29 @@ class Export:
     def gather_material_hook(self, gltf2_material, blender_material, export_settings):
         if self.properties.enabled:
             MSFSMaterial.export(gltf2_material, blender_material, export_settings)
+
+    # mesh hook to change vertex color attributes in vertex colors from Face Corner - Byte Color default to Face Corner Color (byte to float??)
+    # but data looks the same.
+    def gather_mesh_hook(self, gltf2_mesh, blender_mesh, blender_object, vertex_groups, modifiers, skip_filter, materials, export_settings):
+        print("gather_mesh_hookgather_mesh_hook - Started with ", gltf2_mesh, blender_mesh)
+        for col in blender_mesh.color_attributes:
+            print("gather_mesh_hook - col", col, col.data_type, col.domain, col.name)
+            colname_to_remove = ''
+            for coldata in col.data:
+                if col.data_type == 'BYTE_COLOR':
+                    #print("gather_mesh_hook - col data byte", col.data_type, coldata)
+                    colname_to_remove = col.name
+                else:
+                    #print("gather_mesh_hook - col data float (to be skipped)", col.data_type, coldata)
+            colname_to_add = colname_to_remove + "_ASOBO_export_update"
+            if colname_to_remove:
+                print("gather_mesh_hook - col to remove/add", colname_to_remove, colname_to_add)
+                colattr = blender_mesh.color_attributes.new(name=colname_to_add, type='FLOAT_COLOR', domain='CORNER',)
+                print("gather_mesh_hook - loop and add new color attribute data", colname_to_add)
+                print("gather_mesh_hook - range", len(col.data))
+                for v_index in range(len(col.data)):
+                    #print("gather_mesh_hook - color", col.data[v_index].bytecolorattribute.bytecolorattributevalue.color)
+                    print("gather_mesh_hook - color", col.data[v_index].color)
+                    colattr.data[v_index].color = col.data[v_index].color
+                blender_mesh.color_attributes.remove(col)
+        print("gather_mesh_hook - Done")
