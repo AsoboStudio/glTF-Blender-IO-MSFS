1e2d7b4fd3467f2e3b09ab40cc4a5b028fa4beb0
y-khodja <ykhodja@asobostudio.com>
2023-09-12 09:58:17 +0200

    Change regex for reload LODs

--- addons/io_scene_gltf2_msfs/io/msfs_multi_export_objects.py
+++ addons/io_scene_gltf2_msfs/io/msfs_multi_export_objects.py
@@ -77,7 +77,7 @@ class MSFS_OT_ReloadLODGroups(bpy.types.Operator):
 
     @staticmethod
     def get_group_from_name(name):
-        matches = re.findall("^(?i)x[0-9]_|_lod[0-9]+", name)
+        matches = re.findall("^x[0-9]_|_LOD[0-9]+", name)
         # If an object starts with xN_ or ends with _LODN, treat as an LOD
         if matches:
             # Get base object group name from object
